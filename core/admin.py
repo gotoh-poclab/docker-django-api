a503315ddc4ffc4d149f9bc0266c18084d74a700
poclab <hgotoh0820@gmail.com>
2021-09-15 08:27:40 +0900

    adminで登録時間を表示できるように変更

--- core/admin.py
+++ core/admin.py
@@ -3,4 +3,10 @@ from core import models
 
 # Register your models here.
 
-admin.site.register(models.TodoModel)
+# 書かなくても表示されるが、カスタマイズする時には、このように行う。
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("text", "registrationTimeAt")
+
+# 一行登録すると、adminでCRUD操作が行えるようになる。
+# class TodoAdminを書かない場合には、後ろのTodoAdminは必要ない。
+admin.site.register(models.TodoModel, TodoAdmin)
